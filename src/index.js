dd43b8a79c90294ac3a73776b7fb7f3fe86e237a
rentofly <akhil.mishra@rentofly.com>
2025-08-27 00:18:25 +0530

    rotes and controllers for user registration.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {DB_NAME} from "./constants.js";
 import express from "express";
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
-
+import app from "./app.js";
 dotenv.config({path: "./.env"});
 
 connectDB()
